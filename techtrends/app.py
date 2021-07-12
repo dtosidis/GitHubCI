b13928641e70338fc500bae9be8c72cfc477e333
Dimitrios Tosidis <dimtosidis@gmail.com>
2021-07-12 16:57:53 +0200

    app logs
    output logs

--- techtrends/app.py
+++ techtrends/app.py
@@ -3,16 +3,20 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 import logging
+import sys
 
-connection_count = 0
+db_connection_count = 0
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    global connection_count
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    connection_count += 1
-    return connection
+    global db_connection_count
+    try:
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        db_connection_count += 1
+        return connection
+    except Exception as e:
+        logging.error(f'DB exception: {e}')
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -65,7 +69,7 @@ def metrics():
     connection.close()
     post_count = len(posts)
     response = app.response_class(
-            response=json.dumps({"db_connection_count": connection_count, "post_count": post_count}),
+            response=json.dumps({"db_connection_count": db_connection_count, "post_count": post_count}),
             status=200,
             mimetype='application/json'
     )
@@ -77,13 +81,16 @@ def metrics():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.error('Article Not Found')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "' + post['title'] + '" retrieved!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('The "About Us" page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -101,12 +108,14 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('Article "' + title + '" created!')
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    app.run(debug=True,host='0.0.0.0', port='3111')
+    logging.basicConfig(format='%(levelname)s: %(name)-2s - [%(asctime)s] - %(message)s',
+                        level=logging.DEBUG,
+                        handlers= [logging.StreamHandler(sys.stdout), logging.StreamHandler(sys.stderr)])
+    app.run(host='0.0.0.0', port='3111')
